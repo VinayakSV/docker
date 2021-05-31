267f436ebeb3af33e3b3649054e1e8b060dc93c5
Vinayak S Varavate <vinayakvsvs@gmail.com>
2021-05-31 21:50:46 +0530

    First docker application

---
+++ spring-boot-docker/src/main/java/com/vinsv/spring/boot/docker/demo/SpringBootDockerApplication.java
@@ -0,0 +1,22 @@
+package com.vinsv.spring.boot.docker.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class SpringBootDockerApplication {
+
+	
+	@GetMapping("api/message")
+	public String getMessage() {
+		return "Docker message is here!";
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDockerApplication.class, args);
+	}
+
+}
